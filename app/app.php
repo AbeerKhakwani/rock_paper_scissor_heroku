61be0015a19f879ce562a8d48e19e12ecc94ff88
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-04-25 15:21:24 -0700

    Add stuff

--- app/app.php
+++ app/app.php
@@ -1,106 +1,51 @@
 <?php
-    require_once __DIR__ .'/../vendor/autoload.php';
-    require_once __DIR__ .'/../src/Game.php';
-    require_once __DIR__ .'/../src/GameSingle.php';
-
-
-    session_start();
-
-
-
-
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Rectangle.php";
     $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
-
-
-
-
-    $app->get('/', function() use ($app) {
-            return $app['twig']->render("home.twig");
-
+    $app->get("/", function() {
+        return "<a href='/new_rectangle'>New Rectangle</a>";
     });
 
-    $app->post('/singlePlayer', function() use ($app) {
-            return $app['twig']->render("singlePlayer.twig");
-
-    });
-    $app->post('/singleplay', function() use ($app) {
-
-             $person_move= strtolower($_POST['input1']);
-
-             $array=array("rock","paper","scissors");
-
-        if((in_array($person_move, $array)))
-        {
-             $new_Game = new GameSingle;
-             $computor_move= $new_Game->PlayRandom();
-             $GameResult = $new_Game->PlayGame($person_move,$computor_move );
-
-            return $app['twig']->render("singleplay.twig", array("result" =>$GameResult, "computer"=> $computor_move,"you"=> $person_move));
-        }
-        else{
-            return $app['twig']->render("error.twig");
-
-        }
-
-    });
-
-    $app->post('/TwoPlayer', function() use ($app) {
-
-
-            return $app['twig']->render("twoplayers.twig");
-
+    $app->get("/new_rectangle", function() {
+        return "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Make a rectangle!</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Geometry Checker</h1>
+                <p>Enter the dimensions of your rectangle to see if it's a square.</p>
+                <form action='/view_rectangle'>
+                    <div class='form-group'>
+                      <label for='length'>Enter the length:</label>
+                      <input id='length' name='length' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='width'>Enter the width:</label>
+                      <input id='width' name='width' class='form-control' type='number'>
+                    </div>
+                    <button type='submit' class='btn-success'>Create</button>
+                </form>
+            </div>
+        </body>
+        </html>
+        ";
     });
 
+    $app->get("/view_rectangle", function() {
+        $my_rectangle = new Rectangle($_GET['length'], $_GET['width']);
+        $area = $my_rectangle->getArea();
+        if ($my_rectangle->isSquare()) {
+            return "<h1>Congratulations! You made a square! It's area is $area.</h1>";
+        } else {
+            return "<h1>Sorry! This isn't a square. It's area is $area.</h1>";
 
-
-    $app->post('/player2', function() use ($app) {
-
-            $person1_move = strtolower($_POST['input1']);
-
-            $array=array("rock","paper","scissors");
-            if((in_array($person1_move,$array)))
-            {
-            //$_SESSION['player1'] = $person1_move ;
-            $_SESSION['player1']=$person1_move;
-
-            return $app['twig']->render("secondPlayer.twig");
 
         }
-        else{
-            return $app['twig']->render("error.twig");
-
-        }
-
-
-
     });
-
-
-
-    $app->post('/TwoPlayerResult', function() use ($app) {
-
-             $person2_move= strtolower($_POST['input2']);
-
-             $array=array("rock","paper","scissors");
-
-             if((in_array($person2_move,$array)))
-             {
-
-             $new_Game = new Game;
-             $GameResult = $new_Game->PlayGame($_SESSION['player1'],$person2_move );
-
-
-            return $app['twig']->render("twoplayer.twig", array("result" =>$GameResult, "player1"=> $_SESSION['player1'], "player2"=> $person2_move));
-          }
-          else{
-              return $app['twig']->render("error.twig");
-
-          }
-
-    });
-
-
     return $app;
 ?>
