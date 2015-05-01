346fe63c9d4c943d4d4f52e5d4ba62ea701246ee
Abeer Khakwani <abeerkhakwani@hotmail.com>
2015-05-01 13:15:03 -0700

    add gitignore again

--- app/app.php
+++ app/app.php
@@ -1,51 +1,149 @@
 <?php
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Rectangle.php";
-    $app = new Silex\Application();
-
-    $app->get("/", function() {
-        return "<a href='/new_rectangle'>New Rectangle</a>";
-    });
-
-    $app->get("/new_rectangle", function() {
-        return "
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            <title>Make a rectangle!</title>
-        </head>
-        <body>
-            <div class='container'>
-                <h1>Geometry Checker</h1>
-                <p>Enter the dimensions of your rectangle to see if it's a square.</p>
-                <form action='/view_rectangle'>
-                    <div class='form-group'>
-                      <label for='length'>Enter the length:</label>
-                      <input id='length' name='length' class='form-control' type='number'>
-                    </div>
-                    <div class='form-group'>
-                      <label for='width'>Enter the width:</label>
-                      <input id='width' name='width' class='form-control' type='number'>
-                    </div>
-                    <button type='submit' class='btn-success'>Create</button>
-                </form>
-            </div>
-        </body>
-        </html>
-        ";
-    });
-
-    $app->get("/view_rectangle", function() {
-        $my_rectangle = new Rectangle($_GET['length'], $_GET['width']);
-        $area = $my_rectangle->getArea();
-        if ($my_rectangle->isSquare()) {
-            return "<h1>Congratulations! You made a square! It's area is $area.</h1>";
-        } else {
-            return "<h1>Sorry! This isn't a square. It's area is $area.</h1>";
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/Game.php';
+require_once __DIR__ . '/../src/GameSingle.php';
 
 
-        }
-    });
-    return $app;
+session_start();
+
+
+
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views'
+));
+
+
+
+
+$app->get('/', function() use ($app)
+{
+    return $app['twig']->render("home.twig");
+    
+});
+
+$app->get('/singlePlayer', function() use ($app)
+{
+    return $app['twig']->render("singlePlayer.twig");
+    
+});
+$app->post('/singleplay', function() use ($app)
+{
+    
+    $person_move = strtolower($_POST['input1']);
+    
+    $array = array(
+        "rock" ,
+        "paper",
+        "scissors"
+    );
+    $imagesArray = array(
+        "rock" => "../images/rock.png",
+        "paper"=>"../images/paper-md.png",
+        "scissors"=>"../images/scissor.png"
+    );
+    
+    if ((in_array($person_move, $array))) {
+        $new_Game      = new GameSingle;
+        $computor_move = $new_Game->PlayRandom();
+        $GameResult    = $new_Game->PlayGame($person_move, $computor_move);
+        $imageComputor =$imagesArray[$computor_move];
+        $imagesMove=$imagesArray[$person_move];
+
+        return $app['twig']->render("singleplay.twig", array(
+            "result" => $GameResult,
+            "computer" => $computor_move,
+            "you" => $person_move,
+            "imageComputor"=> $imageComputor,
+            "personMove"=>$imagesMove
+        ));
+    } else {
+        return $app['twig']->render("error.twig");
+        
+    }
+    
+});
+
+$app->get('/TwoPlayer', function() use ($app)
+{
+    
+    
+    return $app['twig']->render("twoplayers.twig");
+    
+});
+
+
+
+$app->post('/player2', function() use ($app)
+{
+    
+    $person1_move = strtolower($_POST['input1']);
+    
+    $array = array(
+        "rock",
+        "paper",
+        "scissors"
+    );
+
+    if ((in_array($person1_move, $array))) {
+        //$_SESSION['player1'] = $person1_move ;
+        $_SESSION['player1'] = $person1_move;
+        
+        return $app['twig']->render("secondPlayer.twig");
+        
+    } else {
+        return $app['twig']->render("error.twig");
+        
+    }
+    
+    
+    
+});
+
+
+
+$app->post('/TwoPlayerResult', function() use ($app)
+{
+    
+    $person2_move = strtolower($_POST['input2']);
+    
+    $array = array(
+        "rock",
+        "paper",
+        "scissors"
+    );
+    $imagesArray = array(
+        "rock" => "../images/rock.png",
+        "paper"=>"../images/paper-md.png",
+        "scissors"=>"../images/scissor.png"
+    );
+    
+    if ((in_array($person2_move, $array))) {
+        
+        $new_Game   = new Game;
+        $GameResult = $new_Game->PlayGame($_SESSION['player1'], $person2_move);
+        $player1image =$imagesArray[$_SESSION['player1']];
+        $player2image= $imagesArray[$person2_move];
+        
+        
+        return $app['twig']->render("twoplayer.twig", array(
+            "result" => $GameResult,
+            "player1" => $_SESSION['player1'],
+            "player2" => $person2_move,
+            "player1image"=> $player1image,
+            "player2image"=> $player2image 
+
+        ));
+    } else {
+        return $app['twig']->render("error.twig");
+        
+    }
+    
+});
+
+
+return $app;
 ?>
+ 
